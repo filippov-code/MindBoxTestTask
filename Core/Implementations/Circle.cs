55b50d3a5ab666e828f35b061d15ca40fab2847b
Stepan Filippov <s.filippov@ddplanet.ru>
2024-10-22 21:11:49 +0300

    actualized

---
+++ Core/Implementations/Circle.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Implementations;
+
+public class Circle : IFigure
+{
+    private readonly double _area;
+
+    public double Area => _area;
+    public double Radius { get; init; }
+
+    public Circle(double radius)
+    {
+        if (radius <= 0)
+        {
+            throw new ArgumentException("Радиус должен быть больше 0");
+        }
+
+        Radius = radius;
+        _area = Math.PI * Math.Pow(Radius, 2);
+    }
+}
---
+++ Core/Implementations/Triangle.cs
@@ -0,0 +1,38 @@
+﻿namespace Core.Implementations;
+
+public class Triangle : IFigure
+{
+    private readonly double _area;
+
+    public double Area => _area;
+    public double A { get; init; }
+    public double B { get; init; }
+    public double C { get; init; }
+    public bool IsRightAngled { get; init; }
+
+    public Triangle(double a, double b, double c)
+    {
+        //  Свойство сторон треугольника:
+        //  Сумма длин двух любых сторон треугольника больше длины оставшейся стороны
+        if (a + b <= c || b + c <= a || c + a <= b)
+        {
+            throw new ArgumentException("Невозможно построить треугольник с такими сторонами");
+        }
+
+        A = a;
+        B = b;
+        C = c;
+
+        //  Для проверки на прямоугольность используется теорема Пифагора:
+        //  В прямоугольном треугольнике квадрат гипотенузы равен сумме квадратов катетов треугольника
+        if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a))
+        {
+            IsRightAngled = true;
+        }
+
+        //  Для вычисления площади используется формула Герона:
+        //  S = √(p·(p - a)·(p - b)·(p - c)), p = (a+b+c)/2
+        double p = (A + B + C) / 2;
+        _area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+    }
+}
